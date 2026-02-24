44b89deda9f9c0cb0d3add0d6924dd64eb799cc8
Joseph M  abbinante <joseph.m.abbinante@gmail.com>
2026-02-23 22:51:40 -0600

    modified:   src/components/Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -18,7 +18,7 @@ const Register = ({ onClose }) => {
     setMessage("");
     setLoading(true);
     try {
-      const res = await fetch("/auth/register", {
+      const res = await fetch("/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
